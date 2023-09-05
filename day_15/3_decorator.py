8e4bfdc8d0f4562fc4edb9e8d82d447c3b5a6c04
suyogbhattarai <iamsuyog11@gmail.com>
2023-09-05 16:21:58 +0545

    day_19

--- day_15/3_decorator.py
+++ day_15/3_decorator.py
@@ -1,17 +1,26 @@
 # Decorators are the functions in Python which add extra functionality in the existing function
 # It is created on the basis of closure concept
 
+
 def extra_message(func):
-    def inner_fxn():
-        print("Im learning Python")
-        return func()
+    def inner_fxn(a,b):
+        print("The sum of the numbers is:")
+        return func(a,b)
     return inner_fxn
-
-
 @extra_message
+
+def addition(a,b):
+    print(a+b)
+
+addition(2,3)
+def extra_message2(func):
+     def inner_fxn():
+         print("I am learning python")
+         return func()
+     return inner_fxn
+
+@extra_message2
 def message():
-    print("Hello World")
-
-
-# message = extra_message(message)
+    print("Helllo World!")
 message()
+
--- day_17/1_oop.py
+++ day_17/1_oop.py
@@ -1,12 +1,4 @@
-# What will we study in oop?
-# Class Attribute and Instance Attribute
-# Pillars of OOP
-# => Inheritance, Polymorphism, Encapsulation, Abstraction
-# Types of Inheritance
-# Function / Method Overloading, Operator Overloading, Method Overriding
-# Private, Public and Protected Members
-# Abstract Classes
-# Magic-Methods
+#Basic Introduction--
 
 #OOP is an approach of programming  in which programs are modelled in the real world problems
 #oop stands for object-oriented programming
@@ -27,15 +19,26 @@ class VehicleInformation:
 
     #__init__() function inside a class is called when the object of the class is created.so,this
     #function is also called as a constructor.
-    def __init__(self,number_of_doors,color):
+    def __init__(self,number_of_doors,color): # constructor.Constructor cannot be more than 1 in a class
         self.number_of_doors=number_of_doors # instance attribute
         self.color=color #instance attribute
 
+
+#A method is a function defined inside the class.Methods are called with the class object.
+# They cant be called independently.Here "description" is a method (function defined inside the class)
+
+
+    def description(self): # a method .Method can be of any numbers in a class.
+        return f"The vehicle has {self.number_of_doors} doors and is {self.color} color. It's engine type is"\
+                f"self.engine_type"
+
 v1=VehicleInformation(4,"green") #Creating an object/ instantiating an object
+
 print(v1.engine_type) #objects can access the property and methods of the class using dot(.)
 print(v1.color)
 print(v1.number_of_doors)
-print(VehicleInformation.color) #it raises attribute error because color is not a class attribute it is an
+print(v1.description())
+# print(VehicleInformation.color) #it raises attribute error because color is not a class attribute it is an
 #instance attribute
 
 #We can also access the class attribute using the class.
@@ -45,6 +48,25 @@ print(VehicleInformation.engine_type) #petrol.THIS IS TERMED AS "GETTING"
 v1.engine_type="diesel"#THIS IS TERMED AS "SETTING"
 print(VehicleInformation.engine_type) #PETROL
 print(v1.engine_type)#diesel
+
+
+class BikeInformation:
+    engine_type="petrol" #class attribute
+
+    def __init__(self,no_of_tyres,color):
+        self.no_of_tyres =no_of_tyres #instance attribute
+        self.color = color #instance attribute
+
+v2=BikeInformation(2,"black") #Creating an object /intantiating an object
+print(v2.engine_type)
+print(v2.color)
+print(v2.no_of_tyres)
+
+print(BikeInformation.engine_type) #This is known as getting
+
+v2.engine_type="diesel" #This is termed as setting
+print(v2.engine_type)
+print(BikeInformation.engine_type)
 
 
 
